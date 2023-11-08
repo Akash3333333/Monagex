c061155e49896d6b8e7b8381a5e68a7a22dfbbf5
Hardik_112 <89783619+Hardik-111@users.noreply.github.com>
2023-11-08 17:14:10 +0530

    Added user files

---
+++ frontend/src/components/User/UserProfile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import Sidebar from "../Sidebar";
+// import Feed from "../Feed";
+import './UserProfile.css'
+
+function UserProfile()
+{
+    return(
+        <>
+          <div className="userProfileContainer">
+     HI
+      {/* <Feed /> */}
+    </div>
+        </>
+    )
+};
+
+export default UserProfile;
