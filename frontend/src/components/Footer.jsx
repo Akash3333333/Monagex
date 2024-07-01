e72320367cc051d1d2b71809f268cbd10a832849
Hardik_112 <89783619+Hardik-111@users.noreply.github.com>
2024-07-01 17:08:13 +0530

    added footer files and home styling

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// Footer.js
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="sundar-footer">
+            <div className="footer-content">
+                <p> &copy; 2023 MonageX</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
