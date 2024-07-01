5e711ab6faddf724f50f9480bcb4ed314a38e85d
priyankmi <127470468+priyankmi@users.noreply.github.com>
2024-07-01 15:38:31 +0530

    Deletion for restructure of backend

--- backend/models/Group.js
+++
@@ -1,9 +0,0 @@
-// models/Group.js
-const mongoose = require('mongoose');
-
-const groupSchema = new mongoose.Schema({
-  groupName: { type: String, required: true },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-}, { timestamps: true });
-
-module.exports = mongoose.model('Group', groupSchema);
